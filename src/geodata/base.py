* fixed climatology conversion in GPCC script  * changed behavior of Dataset masking  * turned name and title in Dataset into properties  * exported axesAnnotaiton code from netcdf module into add_strvar in nctools

src/geodata/base.py
@@ -156,7 +156,7 @@ class Variable(object):
         if not ldata and all([len(ax) for ax in axes]): # length not zero
           self.__dict__['shape'] = tuple([len(ax) for ax in axes]) # get shape from axes
       elif all([isinstance(ax,basestring) for ax in axes]):
-        if ldata: axes = [Axis(name=ax, len=n) for ax,n in zip(axes,shape)] # use shape from data
+        if ldata: axes = [Axis(name=ax, length=n) for ax,n in zip(axes,shape)] # use shape from data
         else: axes = [Axis(name=ax) for ax in axes] # initialize without shape
     else: 
       raise VariableError, 'Cannot initialize %s instance \'%s\': no axes declared'%(self.var.__class__.__name__,self.name)
@@ -404,7 +404,7 @@ class Variable(object):
       assert isinstance(mask,np.ndarray) or isinstance(mask,Variable), 'Mask has to be a numpy array or a Variable instance!'
       # 'mask' can be a variable
       if isinstance(mask,Variable):
-        mask = (mask.getArray(unmask=True,axes=self.axes,broadcast=False) > 0) # convert to a boolean numpy array
+        mask = (mask.getArray(unmask=True,axes=self.axes,broadcast=True) > 0) # convert to a boolean numpy array
       assert isinstance(mask,np.ndarray), 'Mask has to be convertible to a numpy array!'      
       # if 'mask' has less dimensions than the variable, it can be extended      
       assert len(self.shape) >= len(mask.shape), 'Data array needs to have the same number of dimensions or more than the mask!'
@@ -427,10 +427,14 @@ class Variable(object):
     if isinstance(self.data_array,ma.MaskedArray):
       # change meta data
       self.__dict__['masked'] = True
-      if fillValue: 
+      if fillValue:
+        # external fill value has priority 
         self.data_array.set_fill_value(fillValue)
         self.__dict__['fillValue'] = fillValue
-      else:  
+      elif self.fillValue:
+        # use fill value we already have
+        self.data_array.set_fill_value(self.fillValue)
+      else:          
         self.__dict__['fillValue'] = self.data_array.get_fill_value() # probably just the default
     
   def unmask(self, fillValue=None):
@@ -637,7 +641,8 @@ class Dataset(object):
       Create a dataset from a list of variables. The basic dataset class has no capability to create variables.
       
       Basic Attributes:
-        name = '' # name of the dataset
+        name = @property # short name (links to name in atts)
+        title = @property # descriptive name (links to name in atts)
         variables = dict() # dictionary holding Variable instances
         axes = dict() # dictionary holding Axis instances (inferred from Variables)
         atts = AttrDict() # dictionary containing global attributes / meta data
@@ -653,6 +658,22 @@ class Dataset(object):
     for var in varlist:
       #print var.name
       self.addVariable(var)
+      
+  @property
+  def name(self):
+    ''' A short name, stored in the atts dictionary. '''
+    return self.atts['name']  
+  @name.setter
+  def name(self, name):
+    self.atts['name'] = name
+  
+  @property
+  def title(self):
+    ''' A long, descriptive name, stored in the atts dictionary. '''
+    return self.atts['title']  
+  @title.setter
+  def title(self, title):
+    self.atts['title'] = title
     
   def addAxis(self, ax):
     ''' Method to add an Axis to the Dataset. If the Axis is already present, check that it is the same. '''
@@ -871,11 +892,24 @@ class Dataset(object):
     for var in self.variables.itervalues():
       var.unload(**kwargs)
       
-  def mask(self, mask=None, **kwargs):
+  def mask(self, mask=None, maskSelf=False, skipList=None, merge=True, **kwargs):
     ''' Apply 'mask' to all variables and add the mask, if it is a variable. '''
-    if isinstance(mask,Variable) and not self.hasVariable(mask): self.addVariable(mask)
+    if skipList is None: skipList = []
+    else: 
+      if not isinstance(skipList,(list,tuple)): raise TypeError
+    # iterate over all variables (not axes!) 
     for var in self.variables.itervalues():
-      if var.ndim >= mask.ndim: var.load(mask=mask, **kwargs)
+      if var.data and var.ndim >= mask.ndim and var.name not in skipList:
+        # need to have data and also the right number of dimensions 
+        lOK = False
+        if isinstance(mask,np.ndarray):
+          if all(mask.shape == var.shape[var.ndim-mask.ndim:]): lOK = True
+        elif isinstance(mask,Variable):
+          if mask is var or mask.name == var.name: lOK = maskSelf # default: False
+          elif all([var.hasAxis(ax) for ax in mask.axes]): lOK = True
+        if lOK: var.mask(mask=mask, merge=merge, **kwargs) # if everything is OK, apply mask
+    # add mask do dataset
+    if isinstance(mask,Variable) and not self.hasVariable(mask): self.addVariable(mask)
     
   def unmask(self, fillValue=None, **kwargs):
     ''' Unmask all Variables in the Dataset. '''

src/geodata/netcdf.py
@@ -18,7 +18,7 @@ import os
 from geodata.base import Variable, Axis, Dataset
 from geodata.misc import checkIndex, isEqual, joinDicts
 from geodata.misc import DatasetError, DataError, AxisError, NetCDFError, PermissionError 
-from geodata.nctools import coerceAtts, writeNetCDF, add_var, add_coord
+from geodata.nctools import coerceAtts, writeNetCDF, add_var, add_coord, add_strvar
 
 def asVarNC(var=None, ncvar=None, mode='rw', axes=None, deepcopy=False, **kwargs):
   ''' Simple function to cast a Variable instance as a VarNC (NetCDF-capable Variable subclass). '''
@@ -415,26 +415,11 @@ class DatasetNetCDF(Dataset):
     
   def axisAnnotation(self, name, strlist, dim):
     ''' Add a list of string values along the specified axis. '''
-    # determine max length of string
-    strlen = 0 
-    for string in strlist: strlen = max(strlen,len(string))
     # figure out dimensions
     dimname = dim if isinstance(dim,basestring) else dim.name
-    dimlen = len(self.axes[dim]) if isinstance(dim,basestring) else len(dim)
-    # allocate array
-    chararray = np.ndarray((dimlen,strlen), dtype='S1')
-    for i in xrange(dimlen):
-      jlen = len(strlist[i])
-      for j in xrange(jlen):
-        chararray[i,j] = strlist[i][j] # unfortunately, direct assignment of sequences does not work
-      # fill remaining with spaces 
-      if jlen < strlen: chararray[i,jlen:] = ' '
+    if len(strlist) != len(self.axes[dim]) if isinstance(dim,basestring) else len(dim): raise AxisError
     # create netcdf dimension and variable
-    dataset = self.dataset
-    dataset.createDimension(name, strlen) # name of month string
-    strvar = dataset.createVariable(name,'S1',(dimname,name))
-    strvar[:] = chararray 
-     
+    add_strvar(self.dataset, name, strlist, dimname)    
     
   def close(self):
     ''' Call this method before deleting the Dataset: close netcdf files; if in write mode, also synchronizes with file system before closing. '''