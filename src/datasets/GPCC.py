* fixed/finished reprojection and resampling process (in CPU)  * also fixed treatment of missing values with ReprojectImage  * added different grid options to NARR and GPCC  TODO:   - make masking optional in regridding   - allow for different interpolation options (by variable)   - collect standard grids in a central location (including axes)

src/datasets/GPCC.py
@@ -10,8 +10,9 @@ This module contains meta data and access functions for the GPCC climatology and
 import netCDF4 as nc # netcdf python module
 import os
 # internal imports
+from geodata.base import Variable
 from geodata.netcdf import DatasetNetCDF, VarNC
-from geodata.gdal import addGDALtoDataset
+from geodata.gdal import addGDALtoDataset, addGDALtoVar
 from geodata.misc import DatasetError
 from geodata.nctools import add_strvar 
 from datasets.misc import translateVarNames, days_per_month, name_of_month, data_root
@@ -114,6 +115,7 @@ if __name__ == '__main__':
 #   reses = ('025',) # hi-res climatology
 #   reses = ('05', '10', '25')
   period = (1979,1981)
+  grid = 'NARR'
   
   # generate averaged climatology
   for res in reses:    
@@ -127,7 +129,6 @@ if __name__ == '__main__':
           
     elif mode == 'convert_climatology':
       
-      from geodata.base import Variable
       from geodata.nctools import writeNetCDF
       
       # load dataset
@@ -184,10 +185,28 @@ if __name__ == '__main__':
       print('   ***   Processing Resolution %s from %s   ***   '%(res,periodstr))
       print('\n')
       source = loadGPCC_TS(varlist=['stations','precip'],resolution=res)
+      # add landmask
+      #print '   ===   landmask   ===   '
+      tmp = source; source.load()
+      tmpatts = dict(name='landmask', units='', long_name='Landmask for Climatology Fields', 
+                description='where this mask is non-zero, no data is available')
+      lnd = Variable(name='landmask', units='', axes=(tmp.lat,tmp.lon), dtype='int16', 
+                    data=tmp.precip.getMask()[0,:,:], atts=tmpatts)
+      lnd = addGDALtoVar(lnd, projection=source.projection, geotransform=source.geotransform)
+      tmp += lnd
       print(source)
       print('\n')
+#       newvar = source.landmask
+#       print
+#       print newvar.name #, newvar.gdal 
+#       print newvar.shape
+#       print newvar.data
+#       print
+      
+            
       # prepare sink
-      filename = avgfile%('_'+res,'_'+periodstr)
+      if grid == 'GPCC': filename = avgfile%('_'+res,'_'+periodstr)
+      if grid == 'NARR': filename = avgfile%('_narr','_'+periodstr)
       if os.path.exists(avgfolder+filename): os.remove(avgfolder+filename)
       atts =dict(period=periodstr, name='GPCC', title='GPCC Climatology') 
       sink = DatasetNetCDF(name='GPCC Climatology', folder=avgfolder, filelist=[filename], atts=source.atts, mode='w')
@@ -203,6 +222,35 @@ if __name__ == '__main__':
       print('   +++   processing climatology   +++   ') 
       CPU.Climatology(period=period[1]-period[0], offset=offset, flush=False)
       print('\n')
+
+      
+      # define new coordinates
+#       import numpy as np
+#       from geodata.base import Axis    
+#       dlon = dlat = 2.5 # resolution
+#       slon, slat, elon, elat =    -179.75, 0.75, -72.25, 85.75
+#       assert (elon-slon) % dlon == 0 
+#       lon = np.linspace(slon+dlon/2,elon-dlon/2,(elon-slon)/dlon)
+#       assert (elat-slat) % dlat == 0
+#       lat = np.linspace(slat+dlat/2,elat-dlat/2,(elat-slat)/dlat)
+#       # add new geographic coordinate axes for projected map
+#       xlon = Axis(coord=lon, atts=dict(name='lon', long_name='longitude', units='deg E'))
+#       ylat = Axis(coord=lat, atts=dict(name='lat', long_name='latitude', units='deg N'))
+
+      # get NARR coordinates
+      if grid == 'NARR':
+        from datasets.NARR import loadNARR_TS, projdict
+        from geodata.gdal import getProjFromDict
+        narr = loadNARR_TS()
+        x = narr.x.copy(deepcopy=True); y = narr.y.copy(deepcopy=True)
+        geot = narr.geotransform
+        proj = getProjFromDict(projdict)
+        # reproject and resample (regrid) dataset
+        print('')
+        print('   +++   processing regidding   +++   ') 
+        CPU.Regrid(projection=proj, geotransform=geot, xlon=x, ylat=y, flush=False)
+        print('\n')
+      
       
 #       # shift longitude axis by 180 degrees  left (i.e. -180 - 180 -> 0 - 360)
 #       print('')
@@ -217,28 +265,31 @@ if __name__ == '__main__':
 #       print('\n')      
 
 
-#       newvar = sink.time
-#       print
-#       print newvar.name, newvar.data 
-#       print newvar.shape
-#       print newvar.coord
-#       print
-
       # get results
-#       CPU.sync(flush=True, deepcopy=False)
+      CPU.sync(flush=True, deepcopy=False)
 #       sink = CPU.getTmp(asNC=True, filename=avgfolder+filename, atts=atts)
-      
+      # print dataset
+#       print('')
+#       print(sink)     
+
       # convert precip data to SI units (mm/s)   
       sink.precip /= (days_per_month.reshape((12,1,1)) * 86400.) # convert in-place
       sink.precip.units = 'kg/m^2/s'      
 
+      newvar = sink.stations
+      print
+      print newvar.name, newvar.masked 
+      print newvar.shape, newvar.data
+      print newvar.data_array.mean()
+      print newvar.data_array.__class__, newvar.fillValue
+            
       # add landmask
       #print '   ===   landmask   ===   '
-      tmpatts = dict(name='landmask', units='', long_name='Landmask for Climatology Fields', 
-                description='where this mask is non-zero, no data is available')
-      sink += VarNC(sink.dataset, name='landmask', units='', axes=(sink.lat,sink.lon), 
-                    data=sink.precip.getMask()[0,:,:], atts=tmpatts)
-      sink.mask(sink.landmask)            
+#       tmpatts = dict(name='landmask', units='', long_name='Landmask for Climatology Fields', 
+#                 description='where this mask is non-zero, no data is available')
+#       sink += VarNC(sink.dataset, name='landmask', units='', axes=(sink.lat,sink.lon), 
+#                     data=sink.precip.getMask()[0,:,:], atts=tmpatts)
+      sink.stations.mask(sink.landmask)            
       # add names and length of months
       sink.axisAnnotation('name_of_month', name_of_month, 'time', 
                           atts=dict(name='name_of_month', units='', long_name='Name of the Month'))

src/datasets/NARR.py
@@ -25,9 +25,10 @@ projdict = dict(proj  = 'lcc', # Lambert Conformal Conic
                 lat_1 =   50., # Latitude of first standard parallel
                 lat_2 =   50., # Latitude of second standard parallel
                 lat_0 =   50., # Latitude of natural origin
-                lon_0 = -107., # Longitude of natural origin
-                x_0   = 5632642.22547, # False Origin Easting
-                y_0   = 4612545.65137) # False Origin Northing
+                lon_0 = -107.) # Longitude of natural origin
+                # 
+                # x_0   = 5632642.22547, # False Origin Easting
+                # y_0   = 4612545.65137) # False Origin Northing
 
 # variable attributes and name
 varatts = dict(air   = dict(name='T2', units='K'), # 2m Temperature
@@ -37,13 +38,15 @@ varatts = dict(air   = dict(name='T2', units='K'), # 2m Temperature
                pevap = dict(name='pet', units='kg/m^2'), # monthly accumulated PET (kg/m^2)
                pr_wtr = dict(name='pwtr', units='kg/m^2'), # total precipitable water (kg/m^2)
                # axes (don't have their own file; listed in axes)
-               lon   = dict(name='lon', units='deg E'), # geographic longitude field
-               lat   = dict(name='lat', units='deg N'), # geographic latitude field
+               lon   = dict(name='lon2D', units='deg E'), # geographic longitude field
+               lat   = dict(name='lat2D', units='deg N'), # geographic latitude field
                time  = dict(name='time', units='days', offset=-1569072, scalefactor=1./24.), # time coordinate
                # N.B.: the time coordinate is only used for the monthly time-series data, not the LTM
-               #       the time offset is chose such that 1979 begins with the origin (time=0)   
-               x     = dict(name='x', units='m', offset=-1*projdict['x_0']), # projected west-east coordinate
-               y     = dict(name='y', units='m', offset=-1*projdict['y_0'])) # projected south-north coordinate
+               #       the time offset is chose such that 1979 begins with the origin (time=0)
+               x     = dict(name='x', units='m', offset=-5632642), # projected west-east coordinate
+               y     = dict(name='y', units='m', offset=-4612545)) # projected south-north coordinate                 
+#                x     = dict(name='x', units='m', offset=-1*projdict['x_0']), # projected west-east coordinate
+#                y     = dict(name='y', units='m', offset=-1*projdict['y_0'])) # projected south-north coordinate
 # N.B.: At the moment Skin Temperature can not be handled this way due to a name conflict with Air Temperature
 # list of variables to load
 ltmvarlist = varatts.keys() # also includes coordinate fields    
@@ -129,7 +132,7 @@ if __name__ == '__main__':
   
 #   mode = 'test_climatology'
   mode = 'average_timeseries'
-  grid = 'NARR'
+  grid = '25'
   period = (1979,1981)
   
   if mode == 'test_climatology':
@@ -160,37 +163,47 @@ if __name__ == '__main__':
     # determine averaging interval
     offset = source.time.getIndex(period[0]-1979)/12 # origin of monthly time-series is at January 1979 
     # initialize processing
-    CPU = CentralProcessingUnit(source, sink, varlist=['precip', 'T2'], tmp=True)
+    CPU = CentralProcessingUnit(source, sink, varlist=['precip', 'T2'], tmp=True) # no need for lat/lon
     
     # start processing climatology
     print('')
     print('   +++   processing climatology   +++   ') 
     CPU.Climatology(period=period[1]-period[0], offset=offset, flush=False)
     print('\n')      
+
+
+    if grid != 'NARR':    
+      # find new coordinate arrays
+      if grid == '025': dlon = dlat = 0.25 # resolution
+      elif grid == '05': dlon = dlat = 0.5
+      elif grid == '10': dlon = dlat = 1.0
+      elif grid == '25': dlon = dlat = 2.5 
+      slon, slat, elon, elat = -179.75, 3.25, -69.75, 85.75
+      assert (elon-slon) % dlon == 0 
+      lon = np.linspace(slon+dlon/2,elon-dlon/2,(elon-slon)/dlon)
+      assert (elat-slat) % dlat == 0
+      lat = np.linspace(slat+dlat/2,elat-dlat/2,(elat-slat)/dlat)
+      # add new geographic coordinate axes for projected map
+      xlon = Axis(coord=lon, atts=dict(name='lon', long_name='longitude', units='deg E'))
+      ylat = Axis(coord=lat, atts=dict(name='lat', long_name='latitude', units='deg N'))
+      # reproject and resample (regrid) dataset
+      print('')
+      print('   +++   processing regidding   +++   ') 
+      print('    ---   (%3.2f,  %3i x %3i)   ---   '%(dlon, len(lon), len(lat)))
+      CPU.Regrid(xlon=xlon, ylat=ylat, flush=False)
+      print('\n')
     
-    # sync temporary storage with output
-    CPU.sync(flush=False)
-    # find new coordinate arrays
-    dlon = dlat = 0.5 # resolution
-#     slon = sink.lon.getArray().min(); elon = sink.lon.getArray().max()
-#     print slon,elon
-#     slat = sink.lat.getArray().min(); elat = sink.lat.getArray().max()
-#     print slat,elat
-    slon, slat, elon, elat =    -179.75, 0.75, -0.25, 85.75 
-    lon = np.linspace(slon+dlon/2,elon-dlon/2,(elon-slon)/dlon)
-    lat = np.linspace(slat+dlat/2,elat-dlat/2,(elat-slat)/dlat)
-    # add new geographic coordinate axes for projected map
-    xlon = Axis(coord=lon, atts=dict(name='lon', long_name='longitude', units='deg E'))
-    ylat = Axis(coord=lat, atts=dict(name='lat', long_name='latitude', units='deg N'))
-    
-    # reproject and resample (regrid) dataset
-    print('')
-    print('   +++   processing regidding   +++   ') 
-    CPU.Regrid(xlon=xlon, ylat=ylat, flush=False)
-    print('\n')      
     
     # sync temporary storage with output
     CPU.sync(flush=True)
+    
+#     newvar = sink.precip
+#     print
+#     print newvar.name, newvar.masked 
+#     print newvar.shape, newvar.data
+#     print newvar.data_array.mean()
+#     print newvar.data_array.__class__
+
 
 #     # make new masks
 #     sink.mask(sink.landmask, maskSelf=False, varlist=['snow','snowh','zs'], invert=True, merge=False)

src/geodata/gdal.py
@@ -11,6 +11,7 @@ and methods to an existing object/class instance.
 '''
 
 import numpy as np
+import numpy.ma as ma
 import types # needed to bind functions to objects
 
 # gdal imports
@@ -200,7 +201,7 @@ def addGDALtoVar(var, projection=None, geotransform=None):
     var.copy = types.MethodType(copy,var)
         
     # define GDAL-related 'class methods'  
-    def getGDAL(self, load=True):
+    def getGDAL(self, load=True, allocate=True, fillValue=None):
       ''' Method that returns a gdal dataset, ready for use with GDAL routines. '''
       if self.gdal and self.projection is not None:
         # determine GDAL data type
@@ -221,6 +222,11 @@ def addGDALtoVar(var, projection=None, geotransform=None):
           if not self.data: raise DataError, 'Need data in Variable instance in order to load data into GDAL dataset!'
           data = self.getArray(unmask=True) # get unmasked data
           data = data.reshape(self.bands,self.mapSize[0],self.mapSize[1]) # reshape to fit bands
+        elif allocate: 
+          if fillValue is None and self.fillValue is not None: fillValue = self.fillValue # use default 
+          if self.fillValue is None: fillValue = ma.default_fill_value(self.dtype)
+          data = np.zeros((self.bands,)+self.mapSize, dtype=self.dtype) + fillValue
+        if load or allocate: 
           # assign data
           for i in xrange(self.bands):
             dataset.GetRasterBand(i+1).WriteArray(data[i,:,:])
@@ -231,9 +237,28 @@ def addGDALtoVar(var, projection=None, geotransform=None):
     # add new method to object
     var.getGDAL = types.MethodType(getGDAL,var)
     
-    def loadGDAL(self, dataset):
+    def loadGDAL(self, dataset, mask=True, fillValue=None):
       ''' Load data from the bands of a GDAL dataset into the variable. '''
-      raise NotImplementedError
+      # check input
+      if not isinstance(dataset,gdal.Dataset): raise TypeError
+      if self.gdal:
+        # get data field
+        if self.bands == 1: data = dataset.ReadAsArray()[:,:] # for 2D fields
+        else: data = dataset.ReadAsArray()[0:self.bands,:,:] # ReadAsArray(0,0,xe,ye)
+#         print data.__class__
+#         print self.masked, self.fillValue
+        # adjust shape (unravel bands)
+        if self.ndim == 2: data = data.squeeze()
+        else: data = data.reshape(self.shape)
+        # shift missing value to zero (for some reason ReprojectImage treats missing values as 0)                      
+        if mask:
+          # mask array where zero (in accord with ReprojectImage convention)
+          if fillValue is None and self.fillValue is not None: fillValue = self.fillValue # use default 
+          if self.fillValue is None: fillValue = ma.default_fill_value(data.dtype)
+          data = ma.masked_values(data, fillValue)
+        # load data)
+        self.load(data=data)
+      # return verification
       return self.data
     # add new method to object
     var.loadGDAL = types.MethodType(loadGDAL,var)    