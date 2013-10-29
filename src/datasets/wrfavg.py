b168900c0734a7afe8386086a17f7fb84660c5b2
Andre R. Erler <aerler@atmosp.physics.utoronto.ca>
2013-10-29 17:58:08 -0400

    * added error handling and reporting/exit code

--- src/datasets/wrfavg.py
+++ src/datasets/wrfavg.py
@@ -43,97 +43,109 @@ def computeClimatology(experiment, filetype, domain, lparallel=False, periods=No
   else:
     pidstr = '' # don't print process ID, sicne there is only one
   
-  # load source
-  fileclass = fileclasses[filetype] # used for target file name
-  print('\n%s   ***   Experiment: \'%s\'   ***   '%(pidstr,experiment) +
-        '\n%s   ***   \'%s\'   ***   '%(pidstr,fileclass.tsfile.format(domain)))
-  source = loadWRF_TS(experiment=experiment, filetypes=[filetype], domains=domain) # comes out as a tuple...
-  if not lparallel: 
-    print(''); print(source); print('')
-  # determine age of oldest source file
-  if not loverwrite:
-    sourceage = datetime.today()
-    for filename in source.filelist:
-      age = datetime.fromtimestamp(os.path.getmtime(filename))
-      sourceage = age if age < sourceage else sourceage
+  ## start actual work: this is inclosed in a try-block, so errors don't 
+  try:
+    
+    raise TypeError
   
-  # figure out start date
-  filebegin = int(source.atts.begin_date.split('-')[0]) # first element is the year
-  fileend = int(source.atts.end_date.split('-')[0]) # first element is the year
-  begindate = offset + filebegin
-  if not ( filebegin <= begindate <= fileend ): raise DateError  
+    # load source
+    fileclass = fileclasses[filetype] # used for target file name
+    print('\n\n{0:s}   ***   Processing Experiment {1:>10s}   ***   '.format(pidstr,"'%s'"%experiment) +
+          '\n{0:s}   ***   {1:^32s}   ***   \n'.format(pidstr,"'%s'"%fileclass.tsfile.format(domain)))
+    source = loadWRF_TS(experiment=experiment, filetypes=[filetype], domains=domain) # comes out as a tuple...
+    if not lparallel: 
+      print(''); print(source); print('')
+    # determine age of oldest source file
+    if not loverwrite:
+      sourceage = datetime.today()
+      for filename in source.filelist:
+        age = datetime.fromtimestamp(os.path.getmtime(filename))
+        sourceage = age if age < sourceage else sourceage
+    
+    # figure out start date
+    filebegin = int(source.atts.begin_date.split('-')[0]) # first element is the year
+    fileend = int(source.atts.end_date.split('-')[0]) # first element is the year
+    begindate = offset + filebegin
+    if not ( filebegin <= begindate <= fileend ): raise DateError  
+    
+    ## loop over periods
+    if periods is None: periods = [begindate-fileend]
+  #   periods.sort(reverse=True) # reverse, so that largest chunk is done first
+    for period in periods:       
+              
+      # figure out period
+      enddate = begindate + period     
+      if filebegin > enddate: raise DateError    
+      if enddate > fileend: 
+        print('\n%s   ---   Invalid Period: End Date %4i not in File!   ---   \n'%(pidstr,enddate))
+        
+      else:  
+        ## begin actual computation
+        periodstr = '%4i-%4i'%(begindate,enddate)
+        avgfolder = root_folder + experiment + '/'
+        print('\n%s   <<<   Computing Climatology from %s on %s grid  >>>   \n'%(pidstr,periodstr,grid))              
   
-  ## loop over periods
-  if periods is None: periods = [begindate-fileend]
-#   periods.sort(reverse=True) # reverse, so that largest chunk is done first
-  for period in periods:       
-            
-    # figure out period
-    enddate = begindate + period     
-    if filebegin > enddate: raise DateError    
-    if enddate > fileend: 
-      print('\n%s   ---   Invalid Period: End Date %4i not in File!   ---   \n'%(pidstr,enddate))
-      
-    else:  
-      ## begin actual computation
-      periodstr = '%4i-%4i'%(begindate,enddate)
-      avgfolder = root_folder + experiment + '/'
-      print('\n%s   <<<   Processing Grid %s from %s   >>>   \n'%(pidstr,grid,periodstr))              
-
-      # determine if sink file already exists, and what to do about it      
-      gridstr = '' if griddef is None or griddef.name is 'WRF' else '_'+griddef.name
-      filename = fileclass.climfile.format(domain,gridstr,'_'+periodstr)
-      if ltest: filename = 'test_' + filename
-      assert os.path.exists(avgfolder)
-      lskip = False # else just go ahead
-      if os.path.exists(avgfolder+filename): 
-        if not loverwrite: 
-          age = datetime.fromtimestamp(os.path.getmtime(avgfolder+filename))
-          # if sink file is newer than source file, skip (do not recompute)
-          if age > sourceage: lskip = True
-          #print sourceage, age
-        if not lskip: os.remove(avgfolder+filename) 
-      
-      # depending on last modification time of file or overwrite setting, start computation, or skip
-      if lskip:        
-        # print message
-        print('%s   >>>   Skipping: File \'%s\' already exists and is newer than source file.   <<<   \n'%(pidstr,filename))              
-      else:
-         
-        # prepare sink
-        sink = DatasetNetCDF(name='WRF Climatology', folder=avgfolder, filelist=[filename], atts=source.atts, mode='w')
-        sink.atts.period = periodstr 
+        # determine if sink file already exists, and what to do about it      
+        gridstr = '' if griddef is None or griddef.name is 'WRF' else '_'+griddef.name
+        filename = fileclass.climfile.format(domain,gridstr,'_'+periodstr)
+        if ldebug: filename = 'test_' + filename
+        assert os.path.exists(avgfolder)
+        lskip = False # else just go ahead
+        if os.path.exists(avgfolder+filename): 
+          if not loverwrite: 
+            age = datetime.fromtimestamp(os.path.getmtime(avgfolder+filename))
+            # if sink file is newer than source file, skip (do not recompute)
+            if age > sourceage: lskip = True
+            #print sourceage, age
+          if not lskip: os.remove(avgfolder+filename) 
         
-        # initialize processing
-        CPU = CentralProcessingUnit(source, sink, varlist=varlist, tmp=True) # no need for lat/lon
-        
-        # start processing climatology
-        CPU.Climatology(period=period, offset=offset, flush=False)
-        
-        # reproject and resample (regrid) dataset
-        if griddef is not None:
-          CPU.Regrid(griddef=griddef, flush=False)
-          print('%s    ---   (%3.2f,  %3i x %3i)   ---   \n'%(pidstr, dlon, len(lon), len(lat)))      
-        
-        # sync temporary storage with output dataset (sink)
-        CPU.sync(flush=True)
-        
-        # make new masks
-        #sink.mask(sink.landmask, maskSelf=False, varlist=['snow','snowh','zs'], invert=True, merge=False)
-        
-        # add names and length of months
-        sink.axisAnnotation('name_of_month', name_of_month, 'time', 
-                            atts=dict(name='name_of_month', units='', long_name='Name of the Month'))
-        sink += VarNC(sink.dataset, name='length_of_month', units='days', axes=(sink.time,), data=days_per_month,
-                      atts=dict(name='length_of_month',units='days',long_name='Length of Month'))
-        
-        # close... and write results to file
-        print('\n%s Writing to: \'%s\'\n'%(pidstr,filename))
-        sink.sync()
-        sink.close()
-        # print dataset
-        if not lparallel:
-          print(''); print(sink); print('')     
+        # depending on last modification time of file or overwrite setting, start computation, or skip
+        if lskip:        
+          # print message
+          print('%s   >>>   Skipping: File \'%s\' already exists and is newer than source file.   <<<   \n'%(pidstr,filename))              
+        else:
+           
+          # prepare sink
+          sink = DatasetNetCDF(name='WRF Climatology', folder=avgfolder, filelist=[filename], atts=source.atts, mode='w')
+          sink.atts.period = periodstr 
+          
+          # initialize processing
+          CPU = CentralProcessingUnit(source, sink, varlist=varlist, tmp=True) # no need for lat/lon
+          
+          # start processing climatology
+          CPU.Climatology(period=period, offset=offset, flush=False)
+          
+          # reproject and resample (regrid) dataset
+          if griddef is not None:
+            CPU.Regrid(griddef=griddef, flush=False)
+            print('%s    ---   (%3.2f,  %3i x %3i)   ---   \n'%(pidstr, dlon, len(lon), len(lat)))      
+          
+          # sync temporary storage with output dataset (sink)
+          CPU.sync(flush=True)
+          
+          # make new masks
+          #sink.mask(sink.landmask, maskSelf=False, varlist=['snow','snowh','zs'], invert=True, merge=False)
+          
+          # add names and length of months
+          sink.axisAnnotation('name_of_month', name_of_month, 'time', 
+                              atts=dict(name='name_of_month', units='', long_name='Name of the Month'))
+          sink += VarNC(sink.dataset, name='length_of_month', units='days', axes=(sink.time,), data=days_per_month,
+                        atts=dict(name='length_of_month',units='days',long_name='Length of Month'))
+          
+          # close... and write results to file
+          print('\n%s Writing to: \'%s\'\n'%(pidstr,filename))
+          sink.sync()
+          sink.close()
+          # print dataset
+          if not lparallel:
+            print(''); print(sink); print('')   
+             
+    # return exit code
+    return 0 # everything OK
+  except Exception:
+    # an error occurred
+    if ldebug: raise # raise error
+    else: return 1 # report error 
 
 if __name__ == '__main__':
   
@@ -142,29 +154,30 @@ if __name__ == '__main__':
   if os.environ.has_key('PYAVG_THREADS'): 
     NP = int(os.environ['PYAVG_THREADS'])
   else: NP = None
-  # only compute whole years 
-  if os.environ.has_key('PYAVG_TEST'): 
-    ltest =  os.environ['PYAVG_TEST'] == 'TEST' 
-  else: ltest = False # i.e. append
+  # run script in debug mode
+  if os.environ.has_key('PYAVG_DEBUG'): 
+    ldebug =  os.environ['PYAVG_DEBUG'] == 'DEBUG' 
+  else: ldebug = False # i.e. append
   # re-compute everything or just update 
   if os.environ.has_key('PYAVG_OVERWRITE'): 
     loverwrite =  os.environ['PYAVG_OVERWRITE'] == 'OVERWRITE' 
-  else: loverwrite = ltest # False means only update old files
+  else: loverwrite = ldebug # False means only update old files
   
-  # defaults
-  if ltest:
+  # default settings
+  if ldebug:
+    #ldebug = False
     NP = NP or 1
-    loverwrite = True
+    #loverwrite = True
     varlist = ['precip', ]
     experiments = ['max']
-#     experiments = ['max','gulf','new','noah'] 
+    #experiments = ['max','gulf','new','noah'] 
     periods = [1,2]
     domains = [1] # domains to be processed
     filetypes = ['srfc',] # filetypes to be processed
     grid = 'WRF' 
   else:
     NP = NP or 4
-    loverwrite = False
+    #loverwrite = False
     varlist = None
     experiments = [] # WRF experiment names (passed through WRFname)
     periods = [5,10] # averaging period
@@ -205,24 +218,50 @@ if __name__ == '__main__':
       for domain in domains:
         # call worker function
         args.append((experiment, filetype, domain))
+        
+  # print an announcement
+  print(datetime.today())
+  print('\n Computing Climatologies for WRF experiments:\n')
+  print(experiments)
+  print('\nTHREADS: %i, TEST: %s, OVERWRITE: %s'%(NP,ldebug,loverwrite))
+  if grid != 'WRF': print('\nRegridding to \'%s\' grid.\n')
 
   ## loop over and process all job sets
+  exitcodes = [] # list of results
   if NP is not None and NP == 1:
     # don't parallelize, if there is only one process: just loop over files    
     for arg in args: # negative pid means serial mode
       experiment, filetype, domain = arg
-      computeClimatology(experiment, filetype, domain, lparallel=False, periods=periods, griddef=griddef)
+      exitcode = computeClimatology(experiment, filetype, domain, lparallel=False, 
+                                    periods=periods, griddef=griddef)
+      exitcodes.append(exitcode)
   else:
     if NP is None: pool = multiprocessing.Pool() 
     else: pool = multiprocessing.Pool(processes=NP)
-    if ltest:
+    if ldebug:
       multiprocessing.log_to_stderr()
       logger = multiprocessing.get_logger()
       logger.setLevel(logging.INFO)    
     # distribute tasks to workers
     for arg in args: # negative pid means serial mode
       experiment, filetype, domain = arg      
-      pool.apply_async(computeClimatology, (experiment, filetype, domain), dict(lparallel=True, periods=periods, griddef=griddef))
+      exitcode = pool.apply_async(computeClimatology, (experiment, filetype, domain), 
+                                  dict(lparallel=True, periods=periods, griddef=griddef))
+      exitcodes.append(exitcode)
     pool.close()
     pool.join()
-  print('')
+
+  # evaluate exit codes    
+  exitcode = 0
+  for ec in exitcodes:
+    assert ec >= 0 
+    exitcode += ec
+  nop = len(args) - exitcode
+  # print summary
+  if exitcode == 0:
+    print('\n   >>>   All {:d} operations completed successfully!!!   <<<   \n'.format(nop))
+  else:
+    print('\n   ===   {:2d} operations completed successfully!    ===   \n'.format(nop) +
+          '\n   ###   {:2d} operations did not complete/failed!   ###   \n'.format(exitcode))
+  print(datetime.today())
+  exit(exitcode)
