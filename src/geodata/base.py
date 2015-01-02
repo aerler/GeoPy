Implemented histogram plot  * based on Axes.hist, using the original sample  * pulled bin computation out into utils.misc  * fixed bug in flat stats tests  * beefed up test for stats tests

src/geodata/base.py
@@ -19,7 +19,7 @@ from plotting.properties import getPlotAtts, variablePlotatts # import plot prop
 from geodata.misc import checkIndex, isEqual, isInt, isNumber, AttrDict, joinDicts, floateps
 from geodata.misc import VariableError, AxisError, DataError, DatasetError, ArgumentError
 from processing.multiprocess import apply_along_axis
-from utils.misc import histogram
+from utils.misc import histogram, binedges
      
 
 class UnaryCheckAndCreateVar(object):
@@ -1050,7 +1050,9 @@ class Variable(object):
     ''' Generate a histogram of along a given axis and preserve the other axes. '''
     # some input checking
     if lflatten and axis is not None: raise ArgumentError
-    if not lflatten and axis is None: raise ArgumentError
+    if not lflatten and axis is None: 
+      if self.ndim > 1: raise ArgumentError
+      else: lflatten = True # treat both as flat arrays...
     if self.dtype.kind in ('S',): 
       if lcheckVar: raise VariableError, "Histogram does not work with string Variables!"
       else: return None
@@ -1062,31 +1064,7 @@ class Variable(object):
       else: return None
     kwargs['density'] = ldensity # overwrite parameter
     # figure out bins
-    if bins is None and binedgs is None: raise ArgumentError
-    elif bins is not None and binedgs is not None:
-      if len(bins)+1 != len(binedgs): raise ArgumentError
-    if bins is not None:
-      # expand bins (values refer to center of bins)
-      if isinstance(bins,(int,np.integer)):
-        if bins == 1: bins = np.asarray(( (self.min()+self.max())/2. ,)) 
-        else: bins = np.linspace(self.min(),self.max(),bins)  
-      elif isinstance(bins,(tuple,list)) and  0 < len(bins) < 4: 
-        bins = np.linspace(*bins)
-      elif not isinstance(bins,(list,np.ndarray)): raise TypeError
-      if len(bins) == 1: 
-        tmpbinedgs = np.asarray((self.min(),self.max()))
-      else:
-        hbd = np.diff(bins) / 2. # make sure this is a float!
-        tmpbinedgs = np.hstack((bins[0]-hbd[0],bins[1:]-hbd,bins[-1]+hbd[-1])) # assuming even spacing
-      if binedgs is None: binedgs = tmpbinedgs # computed from bins
-      elif lcheckVar: assert isEqual(binedgs, np.asarray(tmpbinedgs, dtype=binedgs.dtype))
-    if binedgs is not None:
-      # expand bin edges
-      if not isinstance(binedgs,(tuple,list)): binedgs = np.asarray(binedgs)
-      elif not isinstance(binedgs,np.ndarray): raise TypeError  
-      tmpbins = binedgs[1:] - ( np.diff(binedgs) / 2. ) # make sure this is a float!
-      if bins is None: bins = tmpbins # compute from binedgs
-      elif lcheckVar: assert isEqual(bins, np.asarray(tmpbins, dtype=bins.dtype))
+    bins, binedgs = binedges(bins=bins, binedgs=binedgs, limits=self.limits(), lcheckVar=lcheckVar)
     # setup histogram axis and variable attributes (special case)
     if asVar:
       axatts = self.atts.copy() # variable values become axis
@@ -1151,7 +1129,9 @@ class Variable(object):
     ''' Generate a histogram of along a given axis and preserve the other axes. '''
     # some input checking
     if lflatten and axis is not None: raise ArgumentError
-    if not lflatten and axis is None: raise ArgumentError
+    if not lflatten and axis is None: 
+      if self.ndim > 1: raise ArgumentError
+      else: lflatten = True # treat both as flat arrays...
     if self.dtype.kind in ('S',): 
       if lcheckVar: raise VariableError, "CDF does not work with string Variables!"
       else: return None
@@ -1199,7 +1179,9 @@ class Variable(object):
       ignoreNaN = test.lower() != 'normaltest' or lflatten
     # some input checking
     if lflatten and axis is not None: raise ArgumentError
-    if not lflatten and axis is None: raise ArgumentError
+    if not lflatten and axis is None: 
+      if self.ndim > 1: raise ArgumentError
+      else: lflatten = True # treat both as flat arrays...
     if self.dtype.kind in ('S',): 
       if lcheckVar: raise VariableError, "Statistical tests does not work with string Variables!"
       else: return None

src/geodata/stats.py
@@ -120,9 +120,11 @@ def ks_2samp_wrapper(data, size1=None, ignoreNaN=True):
       underlying (continuous) distribution. This is a wrapper for the SciPy function that 
       removes NaN's, allows application over a field, and only returns the p-value. '''
   if ignoreNaN:
-    nonans = np.invert(np.isnan(data)) # test for NaN's
-    if np.sum(nonans[:size1]) < 3 or np.sum(nonans[size1:]) < 3: return np.NaN # return, if less than 3 non-NaN's
-    data1 = data[nonans[:size1]]; data2 = data[nonans[size1:]] # remove NaN's
+    data1 = data[:size1]; data2 = data[size1:]
+    nonans1 = np.invert(np.isnan(data1)) # test for NaN's
+    nonans2 = np.invert(np.isnan(data2))
+    if np.sum(nonans1) < 3 or np.sum(nonans2) < 3: return np.NaN # return, if less than 3 non-NaN's
+    data1 = data1[nonans1]; data2 = data2[nonans2] # remove NaN's
   else:
     data1 = data[:size1]; data2 = data[size1:]
   # apply test
@@ -149,9 +151,11 @@ def ttest_ind_wrapper(data, size1=None, axis=None, ignoreNaN=True, equal_var=Tru
       are drawn from the same underlying (continuous) distribution. This is a wrapper for the SciPy function that 
       removes NaN's and only returns the p-value (t-test is already vectorized). '''
   if axis is None and ignoreNaN:
-    nonans = np.invert(np.isnan(data)) # test for NaN's
-    if np.sum(nonans[:size1]) < 3 or np.sum(nonans[size1:]) < 3: return np.NaN # return, if less than 3 non-NaN's
-    data1 = data[nonans[:size1]]; data2 = data[nonans[size1:]] # remove NaN's
+    data1 = data[:size1]; data2 = data[size1:]
+    nonans1 = np.invert(np.isnan(data1)) # test for NaN's
+    nonans2 = np.invert(np.isnan(data2))
+    if np.sum(nonans1) < 3 or np.sum(nonans2) < 3: return np.NaN # return, if less than 3 non-NaN's
+    data1 = data1[nonans1]; data2 = data2[nonans2] # remove NaN's
   elif axis is None:
     data1 = data[:size1]; data2 = data[size1:]
   else:
@@ -191,9 +195,11 @@ def mannwhitneyu_wrapper(data, size1=None, ignoreNaN=True, use_continuity=True, 
       underlying (continuous) distribution. This is a wrapper for the SciPy function that 
       removes NaN's, allows application over a field, and only returns the p-value. '''
   if ignoreNaN:
-    nonans = np.invert(np.isnan(data)) # test for NaN's
-    if np.sum(nonans[:size1]) < 3 or np.sum(nonans[size1:]) < 3: return np.NaN # return, if less than 3 non-NaN's
-    data1 = data[nonans[:size1]]; data2 = data[nonans[size1:]] # remove NaN's
+    data1 = data[:size1]; data2 = data[size1:]
+    nonans1 = np.invert(np.isnan(data1)) # test for NaN's
+    nonans2 = np.invert(np.isnan(data2))
+    if np.sum(nonans1) < 3 or np.sum(nonans2) < 3: return np.NaN # return, if less than 3 non-NaN's
+    data1 = data1[nonans1]; data2 = data2[nonans2] # remove NaN's
   else:
     data1 = data[:size1]; data2 = data[size1:]
   # apply test
@@ -222,9 +228,11 @@ def ranksums_wrapper(data, size1=None, ignoreNaN=True):
       underlying (continuous) distribution. This is a wrapper for the SciPy function that 
       removes NaN's, allows application over a field, and only returns the p-value. '''
   if ignoreNaN:
-    nonans = np.invert(np.isnan(data)) # test for NaN's
-    if np.sum(nonans[:size1]) < 3 or np.sum(nonans[size1:]) < 3: return np.NaN # return, if less than 3 non-NaN's
-    data1 = data[nonans[:size1]]; data2 = data[nonans[size1:]] # remove NaN's
+    data1 = data[:size1]; data2 = data[size1:]
+    nonans1 = np.invert(np.isnan(data1)) # test for NaN's
+    nonans2 = np.invert(np.isnan(data2))
+    if np.sum(nonans1) < 3 or np.sum(nonans2) < 3: return np.NaN # return, if less than 3 non-NaN's
+    data1 = data1[nonans1]; data2 = data2[nonans2] # remove NaN's
   else:
     data1 = data[:size1]; data2 = data[size1:]
   # apply test
@@ -262,7 +270,7 @@ def pearsonr_wrapper(data, size1=None, lpval=False, lrho=True, ignoreNaN=True, l
     data1 = data[:size1]; data2 = data[size1:] # find NaN's
     nans1 = np.isnan(data1); nans2 = np.isnan(data2) # remove in both arrays
     nonans = np.invert(np.logical_or(nans1,nans2))
-    if np.sum(nonans) < 3: # return, if less than 3 non-NaN's 
+    if np.sum(nonans) < 3: # return, if too many NaN's 
       if lrho and lpval: return np.zeros(2)+np.NaN
       else: return np.NaN # need to conform to output size
     data1 = data1[nonans]; data2 = data2[nonans] # remove NaN's
@@ -273,6 +281,7 @@ def pearsonr_wrapper(data, size1=None, lpval=False, lrho=True, ignoreNaN=True, l
     data1 = standardize(data1, axis=None, lcopy=False) # apply_stat_test_2samp alread 
     data2 = standardize(data2, axis=None, lcopy=False) #   makes a copy, no need here
   if lsmooth:
+    window_len = min(data1.size,window_len) # automatically shring window
     data1 = smooth(data1,  window_len=window_len, window=window)
     data2 = smooth(data2,  window_len=window_len, window=window)
   if ldetrend:
@@ -317,7 +326,7 @@ def spearmanr_wrapper(data, size1=None, axis=None, lpval=False, lrho=True, ignor
     data1 = data[:size1]; data2 = data[size1:] # find NaN's
     nans1 = np.isnan(data1); nans2 = np.isnan(data2) # remove in both arrays
     nonans = np.invert(np.logical_or(nans1,nans2))
-    if np.sum(nonans) < 3: # return, if less than 3 non-NaN's 
+    if np.sum(nonans) < 3: # return, if too many NaN's 
       if lrho and lpval: return np.zeros(2)+np.NaN
       else: return np.NaN # need to conform to output size
     data1 = data1[nonans]; data2 = data2[nonans] # remove NaN's
@@ -330,6 +339,7 @@ def spearmanr_wrapper(data, size1=None, axis=None, lpval=False, lrho=True, ignor
     data1 = standardize(data1, axis=axis, lcopy=False) # apply_stat_test_2samp alread
     data2 = standardize(data2, axis=axis, lcopy=False) #   makes a copy, no need here
   if lsmooth:
+    window_len = min(data1.size,window_len) # automatically shring window
     data1 = smooth(data1, window_len=window_len, window=window)
     data2 = smooth(data2, window_len=window_len, window=window)
   if ldetrend:
@@ -345,7 +355,7 @@ def spearmanr_wrapper(data, size1=None, axis=None, lpval=False, lrho=True, ignor
 
 
 # generic applicator function for 2 sample statistical tests
-def apply_stat_test_2samp(sample1, sample2, fct=None, axis=None, axis_idx=None, name=None, 
+def apply_stat_test_2samp(sample1, sample2, fct=None, axis=None, axis_idx=None, name=None, laax=True, 
                           lflatten=False, fillValue=None, lpval=True, lrho=False, asVar=None,
                           lcheckVar=True, lcheckAxis=True, pvaratts=None, rvaratts=None, **kwargs):
   ''' Apply a bivariate statistical test or function to two sample Variables and return the result 
@@ -353,7 +363,9 @@ def apply_stat_test_2samp(sample1, sample2, fct=None, axis=None, axis_idx=None, 
       This function can return both, the p-value and the function result (other than the p-value). '''
   # some input checking
   if lflatten and axis is not None: raise ArgumentError
-  if not lflatten and axis is None: raise ArgumentError
+  if not lflatten and axis is None: 
+    if sample2.ndim > 1 and sample2.ndim > 1: raise ArgumentError
+    else: lflatten = True # treat both as flat arrays...
   if asVar is None: asVar = not lflatten
   # check sample vars
   lvar1 = isinstance(sample1, Variable)
@@ -445,10 +457,10 @@ def apply_stat_test_2samp(sample1, sample2, fct=None, axis=None, axis_idx=None, 
   assert lflatten or data1.shape[:-1] == data2.shape[:-1]
   # apply test (serial)
   if lflatten:
-    axis_idx = data1.ndim-1; size1 = data1.shape[-1]; laax=True # shorcuts
+    assert data1.ndim == 1 and data2.ndim == 1
     # merge sample arrays, save dividing index 'size1' (only one argument array per point along axis)
-    data_array = np.concatenate((data1, data2), axis=axis_idx) 
-    res = fct(data_array, size1=size1)
+    data_array = np.concatenate((data1, data2), axis=0) 
+    res = fct(data_array, size1=data1.size) # evaluate function
     # disentagle results
     if lrho and lpval:
       rvar, pvar = res[0],res[1]
@@ -458,10 +470,11 @@ def apply_stat_test_2samp(sample1, sample2, fct=None, axis=None, axis_idx=None, 
       raise NotImplementedError, "Cannot return a single scalar as a Variable object."
   # apply test (parallel)
   else: 
-    axis_idx = data1.ndim-1; size1 = data1.shape[-1]; laax=True # shorcuts
+    axis_idx = data1.ndim-1; size1 = data1.shape[-1] # shorcuts
     # merge sample arrays, save dividing index 'size1' (only one argument array per point along axis)
     data_array = np.concatenate((data1, data2), axis=axis_idx) 
     # select test and set parameters
+    fct = functools.partial(fct, size1=size1)
     res = apply_along_axis(fct, axis_idx, data_array, laax=laax) # apply test in parallel, distributing the data
     # handle masks etc.
     if (lvar1 and sample1.masked) or (lvar2 and sample1.masked): 
@@ -509,6 +522,7 @@ def asDistVar(var, axis='time', dist='KDE', lflatten=False, name=None, atts=None
   varatts['units'] = var.units # these will be the sample units, not the distribution units
   if atts is not None: varatts.update(atts)
   # figure out axes
+  if var.ndim == 1: lflatten = True
   if lflatten:
     iaxis = None; axes = None
   else:
@@ -1177,9 +1191,9 @@ class VarRV(DistVar):
     if isinstance(sample,Variable):
       assert self.axisIndex('params') == sax
       for ax in self.axes[:-1]:
-        if not sample.hasAxis(ax): # last is parameter axis
+        if not sample.hasAxis(ax.name): # last is parameter axis
           raise AxisError, "Sample Variable needs to have a '{:s}' axis.".format(ax.name)
-        if len(sample.getAxis(ax)) != len(ax): # last is parameter axis
+        if len(sample.getAxis(ax.name)) != len(ax): # last is parameter axis
           raise AxisError, "Axis '{:s}' in Sample and DistVar have different length!".format(ax.name)
       sample_data = sample.getArray(unmask=True, fillValue=fillValue, copy=True) # actual data (will be reordered)
       # move extra dimensions to the back

src/geodata_test.py
@@ -545,6 +545,7 @@ class BaseVarTest(unittest.TestCase):
         var = self.var(time=slice(0,10), lat=(50,70), lon=(-130,-110))
       t,x,y = var.axes
     var.standardize(linplace=True) # make variables more likely to test positive
+    
     ## univariate stats tests
     # run simple kstest test
     pval = var.kstest(asVar=False, lflatten=True, dist='norm', args=(0,1))    
@@ -564,46 +565,52 @@ class BaseVarTest(unittest.TestCase):
     # fit normal distribution over entire range 
     nvar = var.norm(axis=t.name, lflatten=True, ldebug=False)
     pval = nvar.kstest(var.data_array.ravel(), asVar=False)
-#     print pval
     assert pval >= 0.
     assert pval.shape == nvar.shape[:-1]
     xvar = var.genextreme(axis=t.name, lflatten=False, ldebug=False)
-#     print xvar
     pvar = xvar.kstest(var)
     assert pvar.shape == xvar.shape[:-1]
     assert np.all(pvar.data_array >= 0)
-#     print pvar
-#     print pvar.data_array
+
+    rav = var.copy(); sin = rav.sin(); cos = var.cos()
+    rnd = var.copy(); rnd.data_array = np.random.randn(var.data_array.size).reshape(var.shape)
     ## bivariate stats tests
-    rav = var.copy()
     pval = kstest(var, rav, lflatten=True)
-    assert pval > 0.99 # this will usually be close to zero, since none of these are normally distributed
-#     pval = ttest(var, rav, lflatten=True)  
-    pval = mwtest(var, rav, lflatten=True)  
-#     pval = wrstest(var, rav, lflatten=True)  
-    #print pval
-    assert pval > 0.99 # this will usually be close to zero, since none of these are normally distributed
-#     pvar = kstest(var, rav, axis='time')    
+    assert pval > 0.95 # this will usually be close to zero, since none of these are normally distributed
     pvar = ttest(var, rav, axis='time')    
-    assert np.all(pvar.data_array > 0.95) # not all tests are that accurate...
-    assert pvar.shape == var.shape[1:] # this will usually be close to zero, since none of these are normally distributed
-#     pvar = mwtest(var, rav, axis='time')
-    pvar = wrstest(var, rav, axis='time')
     #print pvar
     #print pvar.data_array
-    assert np.all(pvar.data_array > 0.95) # not all tests are that accurate...
+    assert pvar.data_array.mean() > 0.95 # not all tests are that accurate...
     assert pvar.shape == var.shape[1:] # this will usually be close to zero, since none of these are normally distributed
+    # reverse tests: now all should be negative
+    assert not isEqual(sin.data_array,cos.data_array)
+    pval = mwtest(sin, cos, lflatten=True)  
+    assert pval < 0.05 # this will usually be close to zero, since none of these are normally distributed
+    pvar = wrstest(sin, cos, axis='time')
+    assert pvar.data_array.mean() < 0.5 # not all tests are that accurate...
+    assert pvar.shape == var.shape[1:] # this will usually be close to zero, since none of these are normally distributed
+    
     ## correlation coefficients
     rho,pval = pearsonr(var, rav, lpval=True, lrho=True, lflatten=True, lstandardize=True, lsmooth=True, window_len=5)
-    #rho,pval = spearmanr(var, rav, lpval=True, lrho=True, lflatten=True)
     #print rho, pval
     assert rho > 0.99
     assert pval < 0.01 # this will usually be close to zero, since none of these are normally distributed
-    #rvar,pvar = pearsonr(var, rav, lpval=True, lrho=True, axis='time')
     rvar,pvar = spearmanr(var, rav, lpval=True, lrho=True, axis='time', lstandardize=True, lsmooth=True)
+    assert rvar.data_array.mean() > 0.95 # not all tests are that accurate...
+    assert pvar.data_array.mean() < 0.05 # not all tests are that accurate...
+    assert rvar.shape == var.shape[1:] # this will usually be close to zero, since none of these are normally distributed
+    # and now reverse
+    rho,pval = spearmanr(var, rnd, lpval=True, lrho=True, lflatten=True)
+    #print rho, pval
+    assert rho < 0.5
+    assert pval > 0.05 # this will usually be close to zero, since none of these are normally distributed
+    rvar,pvar = pearsonr(var, rnd, lpval=True, lrho=True, axis='time')
     #print rvar
-    #print rho.data_array
-    assert np.all(rvar.data_array > 0.00) # not all tests are that accurate...
+    #print rvar.data_array.mean()
+    #print pvar
+    #print pvar.data_array.mean()
+    assert rvar.data_array.mean() < 0.25 # not all tests are that accurate...
+    assert pvar.data_array.mean() > 0.25 # not all tests are that accurate...
     assert rvar.shape == var.shape[1:] # this will usually be close to zero, since none of these are normally distributed
     
   def testUnaryArithmetic(self):
@@ -1321,7 +1328,7 @@ if __name__ == "__main__":
         
     specific_tests = None
 #     specific_tests = ['ReductionArithmetic']
-    specific_tests = ['DistributionVariables']
+#     specific_tests = ['DistributionVariables']
 #     specific_tests = ['Ensemble']
 #     specific_tests = ['StatsTests']    
 
@@ -1332,10 +1339,10 @@ if __name__ == "__main__":
     tests += ['NetCDFVar']
     tests += ['GDALVar']
     # list of dataset tests
-#     tests += ['BaseDataset']
-#     tests += ['DatasetNetCDF']
-#     tests += ['DatasetGDAL']
-      
+    tests += ['BaseDataset']
+    tests += ['DatasetNetCDF']
+    tests += ['DatasetGDAL']
+#       
     
     # construct dictionary of test classes defined above
     test_classes = dict()

src/plotting/axes.py
@@ -14,10 +14,10 @@ from matplotlib.axes import Axes
 from mpl_toolkits.axes_grid.axes_divider import LocatableAxes
 # internal imports
 from geodata.base import Variable, Ensemble
-from geodata.misc import ListError, AxisError
+from geodata.misc import ListError, AxisError, ArgumentError, isEqual
 from plotting.misc import smooth, getPlotValues
 from collections import OrderedDict
-
+from utils.misc import binedges
 
 ## new axes class
 class MyAxes(Axes): 
@@ -30,15 +30,16 @@ class MyAxes(Axes):
   flipxy       = False
   xname        = None
   xunits       = None
-  xpad         =  2 
+  xpad         = 2 
   yname        = None
   yunits       = None
-  ypad         = -2
+  ypad         = 0
   
-  def linePlot(self, varlist, linestyles=None, varatts=None, legend=None, xticks=True, yticks=True,
-               hline=None, vline=None, title=None, flipxy=None, xlabel=True, ylabel=True, xlim=None,
-               xlog=False, ylog=False, ylim=None, lsmooth=False, lprint=False, **kwargs):
-    ''' A function to draw a list of 1D variables into an axes, and annotate the plot based on variable properties. '''
+  def linePlot(self, varlist, linestyles=None, varatts=None, legend=None, llabel=True, labels=None, xticks=True, 
+               yticks=True, hline=None, vline=None, title=None, reset_color=True, flipxy=None, xlabel=True, ylabel=True,
+               xlog=False, ylog=False, xlim=None, ylim=None, lsmooth=False, lprint=False, **kwargs):
+    ''' A function to draw a list of 1D variables into an axes, 
+        and annotate the plot based on variable properties. '''
     # varlist is the list of variable objects that are to be plotted
     #print varlist
     if isinstance(varlist,Variable): varlist = [varlist]
@@ -73,6 +74,12 @@ class MyAxes(Axes):
       varname,varunits,axname,axunits = self.xname,self.xunits,self.yname,self.yunits
     else:
       axname,axunits,varname,varunits = self.xname,self.xunits,self.yname,self.yunits
+    # reset color cycle
+    if reset_color: self.set_color_cycle(None)
+    # prepare label list
+    if labels is None: labels = []; lmklblb = True
+    elif len(labels) == len(varlist): lmklblb = False
+    else: raise ArgumentError, "Incompatible length of label list."
     # loop over variables
     plts = [] # list of plot handles
     if self.variables is None: self.variables = OrderedDict()
@@ -85,14 +92,17 @@ class MyAxes(Axes):
       if var.plot is not None and varax.plot is not None: 
         if varax.units != axunits and var.plot.preserve == 'area':
           val /= varax.plot.scalefactor
-      # save variable  
-      if lname: label = var.name # default label: variable name
-      elif ldataset: label = var.dataset.name
-      else: label = n
+      # save variable 
+      if lmklblb: 
+        if lname: label = var.name # default label: variable name
+        elif ldataset: label = var.dataset.name
+        else: label = n
+        labels.append(label)
+      else: label = labels[n]
       self.variables[label] = var
       # figure out keyword options
       kwatts = kwargs.copy(); kwatts.update(varatt) # join individual and common attributes     
-      if 'label' not in kwatts: kwatts['label'] = label
+      if llabel: kwatts['label'] = label
       # N.B.: other scaling behavior could be added here
       if lprint: print varname, varunits, val.mean()    
       if lsmooth: val = smooth(val)
@@ -139,6 +149,111 @@ class MyAxes(Axes):
     if vline is not None: self.addVline(vline)
     # return handle
     return plts
+
+  def histogram(self, varlist, bins=None, binedgs=None, histtype='bar', lstacked=False, lnormalize=True,
+                lcumulative=0, varatts=None, legend=None, colors=None, llabel=True, labels=None, align='mid', rwidth=None, 
+                bottom=None, weights=None, xticks=True, yticks=True, hline=None, vline=None, title=None, reset_color=True, 
+                flipxy=None, xlabel=True, ylabel=True, log=False, xlim=None, ylim=None, lprint=False, **kwargs):
+    ''' A function to draw histograms of a list of 1D variables into an axes, 
+        and annotate the plot based on variable properties. '''
+    # varlist is the list of variable objects that are to be plotted
+    #print varlist
+    if isinstance(varlist,Variable): varlist = [varlist]
+    elif not isinstance(varlist,(tuple,list,Ensemble)) or not all([isinstance(var,Variable) for var in varlist]): raise TypeError
+    for var in varlist: var.squeeze() # remove singleton dimensions
+    # varatts are variable-specific attributes that are parsed for special keywords and then passed on to the
+    if varatts is None: varatts = [dict()]*len(varlist)  
+    elif isinstance(varatts,dict):
+      tmp = [varatts[var.name] if var.name in varatts else dict() for var in varlist]
+      if any(tmp): varatts = tmp # if any variable names were found
+      else: varatts = [varatts]*len(varlist) # assume it is one varatts dict, which will be used for all variables
+    elif not isinstance(varatts,(tuple,list)): raise TypeError
+    if not all([isinstance(atts,dict) for atts in varatts]): raise TypeError
+    # check axis: they need to have only one axes, which has to be the same for all!
+    if len(varatts) != len(varlist): raise ListError, "Failed to match varatts to varlist!"  
+    # line/plot label policy
+    lname = not any(var.name == varlist[0].name for var in varlist[1:])
+    if lname or not all(var.dataset is not None for var in varlist): ldataset = False
+    elif not any(var.dataset.name == varlist[0].dataset.name for var in varlist[1:]): ldataset = True
+    else: ldataset = False
+    # initialize axes names and units
+    self.flipxy = flipxy
+    if not self.flipxy: # histogram has opposite convention
+      varname,varunits,axname,axunits = self.xname,self.xunits,self.yname,self.yunits
+    else:
+      axname,axunits,varname,varunits = self.xname,self.xunits,self.yname,self.yunits
+    # reset color cycle
+    if reset_color: self.set_color_cycle(None)
+    # figure out bins
+    vmin = np.min([var.min() for var in varlist])
+    vmax = np.max([var.max() for var in varlist])
+    bins, binedgs = binedges(bins=bins, binedgs=binedgs, limits=(vmin,vmax), lcheckVar=True)
+    # prepare label list
+    if labels is None: labels = []; lmklblb = True
+    elif len(labels) == len(varlist): lmklblb = False
+    else: raise ArgumentError, "Incompatible length of label list."
+    if self.variables is None: self.variables = OrderedDict()
+    # loop over variables
+    values = [] # list of plot handles
+    for n,var in zip(xrange(len(varlist)),varlist):
+      # scale variable values(axes are irrelevant)
+      val, varunits, varname = getPlotValues(var, checkunits=varunits, checkname=None)
+      val = val.ravel() # flatten array
+      if not varname.endswith('_bins'): varname += '_bins'
+      # figure out label
+      if lmklblb:
+        if lname: label = var.name # default label: variable name
+        elif ldataset: label = var.dataset.name
+        else: label = n
+        labels.append(label)
+      else: label = labels[n]
+      # save variable  
+      self.variables[label] = var
+      if lprint: print varname, varunits, val.mean()    
+      # save values
+      values.append(val)
+    # figure out orientation
+    if self.flipxy: orientation = 'horizontal' 
+    else: orientation = 'vertical'
+    # call histogram method of Axis
+    if not llabel: labels = None 
+    hdata, bin_edges, patches = self.hist(values, bins=binedgs, normed=lnormalize, weights=weights, cumulative=lcumulative, 
+                                          bottom=bottom, histtype=histtype, align=align, orientation=orientation, 
+                                          rwidth=rwidth, log=log, color=colors, label=labels, stacked=lstacked, **kwargs)
+    del hdata; assert isEqual(bin_edges, binedgs)
+    # N.B.: generally we don't need to keep the histogram results - there are other functions for that
+    # set axes limits
+    if isinstance(xlim,(list,tuple)) and len(xlim)==2: self.set_xlim(*xlim)
+    elif xlim is not None: raise TypeError
+    if isinstance(ylim,(list,tuple)) and len(ylim)==2: self.set_ylim(*ylim)
+    elif ylim is not None: raise TypeError 
+    # set title
+    if title is not None: self.addTitle(title)
+    # set axes labels  
+    if not self.flipxy: 
+      self.xname,self.xunits,self.yname,self.yunits = varname,varunits,axname,axunits
+    else: 
+      self.xname,self.xunits,self.yname,self.yunits = axname,axunits,varname,varunits
+    # format axes ticks
+    self.xTickLabels(xticks, loverlap=False)
+    self.yTickLabels(yticks, loverlap=False)
+    # format axes labels
+    self.xLabel(xlabel)
+    self.yLabel(ylabel)    
+    # N.B.: a typical custom label that makes use of the units would look like this: 'custom label [{1:s}]', 
+    # where {} will be replaced by the appropriate default units (which have to be the same anyway)
+    # make monthly ticks
+    if self.xname == 'time' and self.xunits == 'month':
+      if len(xticks) == 12 or len(xticks) == 13:
+        self.xaxis.set_minor_locator(mpl.ticker.AutoMinorLocator(2)) # self.minorticks_on()
+    # add legend
+    if isinstance(legend,dict): self.addLegend(**legend) 
+    elif isinstance(legend,(int,np.integer,float,np.inexact)): self.addLegend(loc=legend)
+    # add orientation lines
+    if hline is not None: self.addHline(hline)
+    if vline is not None: self.addVline(vline)
+    # return handle
+    return bins, patches # bins can be used as support for distributions
   
   def addHline(self, hline, **kwargs):
     ''' add one or more horizontal lines to the plot '''

src/plotting_test.py
@@ -26,6 +26,7 @@ from plotting.mapplots import srfcPlot
 from plotting.figure import getFigAx
 # use common MPL instance
 from plotting.misc import loadMPL
+from geodata.stats import mwtest, kstest, wrstest
 mpl,pyl = loadMPL(linewidth=1.)
 
 
@@ -66,7 +67,7 @@ class LinePlotTest(unittest.TestCase):
     assert not isinstance(ax,(list,tuple)) # should return a "naked" axes
     var1 = self.var1; var2 = self.var2
     # create plot
-    plts = ax.linePlot([var1, var2], ylabel='custom label [{1:s}]', 
+    plts = ax.linePlot([var1, var2], ylabel='custom label [{1:s}]', llabel=False, 
                        ylim=var1.limits(), legend=2, hline=2., vline=(2,3))
     assert len(plts) == 2
     # add label
@@ -120,6 +121,58 @@ class LinePlotTest(unittest.TestCase):
     # add a line
     ax.addHline(3)
     
+    
+class BarPlotTest(unittest.TestCase):  
+   
+  def setUp(self):
+    ''' create two test variables '''
+    # create axis and variable instances (make *copies* of data and attributes!)
+    x1 = np.random.randn(30); xax1 = Axis(name='X1-Axis', units='X Units', length=len(x1)) 
+    var1 = Variable(axes=(xax1,), data=x1.copy(), atts=dict(name='blue', units='units'))
+    self.var1 = var1; self.xax1 = xax1
+    x2 = np.random.randn(50); xax2 = Axis(name='X2-Axis', units='X Units', length=len(x2))
+    var2 = Variable(name='green',units='units',axes=(xax2,), data=x2)
+    self.var2 = var2; self.xax2 = xax2
+    # add to list
+    self.vars = [var1, var2]
+    self.axes = [xax1, xax2]
+        
+  def tearDown(self):
+    ''' clean up '''
+    for var in self.vars:     
+      var.unload() # just to do something... free memory
+    for ax in self.axes:
+      ax.unload()
+    
+  ## basic tests every variable class should pass
+
+  def testBasicHistogram(self):
+    ''' test a simple line plot with two lines '''    
+    fig,ax = getFigAx(1, name=sys._getframe().f_code.co_name[4:], mpl=mpl) # use test method name as title
+    assert fig.__class__.__name__ == 'MyFigure'
+    assert fig.axes_class.__name__ == 'MyAxes'
+    assert not isinstance(ax,(list,tuple)) # should return a "naked" axes
+    # settings
+    varlist = [self.var1, self.var2]
+    nbins = 10
+    # create regular histogram
+    bins, ptchs = ax.histogram(varlist, bins=nbins, legend=2, alpha=0.5, rwidth=0.8)
+    assert len(ptchs) == 2
+    assert len(bins) == nbins
+    vmin = np.min([var.min() for var in varlist])
+    vmax = np.max([var.max() for var in varlist])
+    #print bins[0], vmin; print bins[-1], vmax
+    assert bins[0] == vmin and bins[-1] == vmax
+    # add a KDE plot
+    support = np.linspace(vmin, vmax, 100)
+    kdevars = [var.kde(lflatten=True).histogram(support=support) for var in varlist]
+    ax.linePlot(kdevars, linewidth=2)
+    # add label
+    pval = kstest(varlist[0], varlist[1], lflatten=True) 
+    pstr = 'p-value = {:3.2f}'.format(pval)
+    ax.addLabel(label=pstr, loc=1, lstroke=False, lalphabet=True, size=None, prop=None)
+
+    
 if __name__ == "__main__":
 
     
@@ -131,6 +184,7 @@ if __name__ == "__main__":
     tests = [] 
     # list of variable tests
     tests += ['LinePlot'] 
+    tests += ['BarPlot']
     
 
     # construct dictionary of test classes defined above