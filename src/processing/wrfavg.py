8fccefc892e3d6c6b8ef6dfeb2329c8c20db6891
Andre R. Erler <A.R.Erler@gmail.com>
2016-06-21 17:27:36 -0400

    changed exit code from np.ceil to int in processors

--- src/processing/wrfavg.py
+++ src/processing/wrfavg.py
@@ -297,4 +297,4 @@ if __name__ == '__main__':
   # call parallel execution function
   ec = asyncPoolEC(computeClimatology, args, kwargs, NP=NP, ldebug=ldebug, ltrialnerror=True)
   # exit with fraction of failures (out of 10) as exit code
-  exit(int(10+np.ceil(10.*ec/len(args))) if ec > 0 else 0)
+  exit(int(10+int(10.*ec/len(args))) if ec > 0 else 0)
