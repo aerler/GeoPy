some more updates to BC and fix for radiation units in NRCan

src/datasets/NRCan.py
@@ -47,7 +47,8 @@ varatts = dict(Tmax    = dict(name='Tmax', units='K'), # 2m maximum temperature
                pet     = dict(name='pet', units='kg/m^2/s'), # potential evapo-transpiration
                liqprec = dict(name='liqprec', units='kg/m^2/s'), # total precipitation
                snowh   = dict(name='snowh', units='m'), # snow depth
-               SWD     = dict(name='SWD', units='J/m^2/s'), # solar radiation
+               SWD     = dict(name='SWDNB', units='W/m^2', scalefactor=30.4e6), # solar radiation, corrected (MJ/day->J/month)
+               SWDNB   = dict(name='SWDNB', units='W/m^2'), # solar radiation
                # diagnostic variables
                T2      = dict(name='T2', units='K'), # 2m average temperature
                solprec = dict(name='liqprec', units='kg/m^2/s'), # total precipitation
@@ -60,7 +61,7 @@ varatts = dict(Tmax    = dict(name='Tmax', units='K'), # 2m maximum temperature
                lon  = dict(name='lon', units='deg E'), # geographic longitude field
                lat  = dict(name='lat', units='deg N')) # geographic latitude field
 
-tsvaratts = varatts
+tsvaratts = varatts.copy()
 # list of variables to load
 varlist = varatts.keys() # also includes coordinate fields    
 # variable and file lists settings
@@ -172,11 +173,11 @@ from utils.ascii import rasterDataset
 
 # Normals (long-term means): ASCII data specifications
 norm_defaults = dict(axes=('time',None,None), dtype=np.float32)
-norm_vardefs = dict(maxt = dict(grid='NA12', name='Tmax', units='K', offset=273.15, **norm_defaults), # 2m maximum temperature
+norm_vardefs = dict(maxt = dict(grid='NA12', name='Tmax', units='K', offset=273.15, **norm_defaults), # 2m maximum temperature, originally in degrees Celsius
                     mint = dict(grid='NA12', name='Tmin', units='K', offset=273.15, **norm_defaults), # 2m minimum temperature
                     pcp  = dict(grid='NA12', name='precip', units='kg/m^2/month', transform=transformMonthly, **norm_defaults), # total precipitation
                     pet  = dict(grid='NA12', name='pet', units='kg/m^2/month', transform=transformMonthly, **norm_defaults), # potential evapo-transpiration
-                    rrad = dict(grid='NA12', name='SWD', units='J/m^2/month', transform=transformMonthly, **norm_defaults), # solar radiation
+                    rrad = dict(grid='NA12', name='SWDNB', units='W/m^2', scalefactor=1e6/86400., **norm_defaults), # solar radiation, originally in MJ/m^2/day
                     rain = dict(grid='CA12', name='liqprec', units='kg/m^2/month', transform=transformMonthly, **norm_defaults), # total precipitation
                     snwd = dict(grid='CA12', name='snowh', units='m', scalefactor=1./100., **norm_defaults), ) # snow depth
 norm_axdefs = dict(time = dict(name='time', units='month', coord=np.arange(1,13)),) # time coordinate
@@ -391,9 +392,9 @@ loadShapeTimeSeries = loadNRCan_ShpTS # time-series without associated grid (e.g
 
 if __name__ == '__main__':
   
-#     mode = 'test_climatology'
+    mode = 'test_climatology'
 #     mode = 'test_timeseries'
-    mode = 'test_point_climatology'
+#     mode = 'test_point_climatology'
 #     mode = 'test_point_timeseries'
 #     mode = 'convert_Normals'
     pntset = 'glbshp' # 'ecprecip'
@@ -404,12 +405,12 @@ if __name__ == '__main__':
             
         # load averaged climatology file
         print('')
-        dataset = loadNRCan(grid=grid,period=period,resolution=res)
+        dataset = loadNRCan(grid=grid,period=period,resolution=res, varatts=dict(pet=dict(name='pet_wrf')))
         print(dataset)
         print('')
         print(dataset.geotransform)
-        print(dataset.precip.getArray().mean())
-        print(dataset.precip.masked)
+        print(dataset.SWDNB.mean())
+        print(dataset.SWDNB.masked)
         
         # print time coordinate
         print

src/processing/bc_methods.py
@@ -17,7 +17,7 @@ from geodata.netcdf import DatasetNetCDF, VarNC
 
 # some helper stuff for validation
 eps = np.finfo(np.float32).eps # single precision rounding error is good enough
-Stats = col.namedtuple('Stats', ('Bias','RMSE','Corr'))
+Stats = col.namedtuple('Stats', ('Correction','RMSE','Correlation','Bias',))
 
 def getBCmethods(method, **bcargs):
     ''' function that returns an instance of a specific BiasCorrection child class specified as method; 
@@ -155,12 +155,13 @@ class BiasCorrection(object):
                 stats = None
             else:
                 delta = bcvar.data_array - obsvar.data_array
+                correction = np.mean(self._correction[varname]) # can be scalar 
                 bias = delta.mean()
                 if bias < eps: bias = 0.
                 rmse = np.asscalar(np.sqrt(np.mean(delta**2)))
                 if rmse < eps: rmse = 0.
-                corr = np.corrcoef(bcvar.data_array.flatten(),obsvar.data_array.flatten())[0,1]
-                stats = Stats(Bias=bias,RMSE=rmse,Corr=corr)
+                correlation = np.corrcoef(bcvar.data_array.flatten(),obsvar.data_array.flatten())[0,1]
+                stats = Stats(Correction=correction,RMSE=rmse,Correlation=correlation,Bias=bias,)
             if lprint: print(varname,stats)
             validation[varname] = stats
         # return fit statistics