fixed PET calculation (elevation is a scaler)
  * fixed loading of scalar variables in DatasetNetCDF

src/datasets/CORDEX.py
@@ -284,7 +284,7 @@ def loadCORDEX_Raw(varlist=None, varname=None, dataset=None, folder=folder_patte
 def loadCORDEX_TS(dataset=None, varlist=None, grid=None, station=None, shape=None, bias_correction=None,
                   dataset_subfolder=None, folder=folder_pattern, filename=filename_pattern,
                   domain='NAM-22', aggregation='mon', scenario='historical',
-                  varatts=None, lxarray=True, lgeoref=False, lraw=False, **kwargs):
+                  varatts=None, lxarray=True, lgeoref=False, lraw=False, lscalars=True, **kwargs):
     ''' function to load a consolidated CORDEX dataset '''
     # figure out dataset attributes
     ds_atts = datasetAttributes(dataset, **kwargs)
@@ -314,7 +314,8 @@ def loadCORDEX_TS(dataset=None, varlist=None, grid=None, station=None, shape=Non
             proj4_string = readCFCRS(xds, lraise=True, lproj4=True)
             ds = addGeoReference(xds, proj4_string=proj4_string)
     else:
-        ds = DatasetNetCDF(name=dataset, filelist=[filepath], varlist=varlist, varatts=varatts, **kwargs)
+        ds = DatasetNetCDF(name=dataset, filelist=[filepath], varlist=varlist, varatts=varatts,
+                           lscalars=lscalars, **kwargs)
         if lgeoref: 
             raise NotImplementedError
     # return formated dataset
@@ -359,44 +360,56 @@ if __name__ == '__main__':
     
                              
     if mode == 'compute_forcing':
+      
+        for dataset in dataset_list:
+          
+            ds_atts = datasetAttributes(dataset)
+            if ds_atts.reanalysis: scenarios = ('evaluation',)
+            else: scenarios = ('historical','rcp85') 
+            
+            for scenario in scenarios:
+
+                print("\n   ***   ",dataset,scenario,"   ***   \n")
        
-        pet_varlist = ['T2','Tmin','Tmax','pmsl','zs','q2','U10','SWDNB','SWUPB','LWDNB','LWUPB']
-        lwf_varlist = ['snow','precip']
-        #dataset = 'CanESM-CRCM5'; scenario = 'historical'
-        ds = loadCORDEX_TS(dataset=dataset, grid=station_name, varlist=lwf_varlist+pet_varlist, 
-                           lxarray=False, load=True, mode='rw',
-                           dataset_subfolder=station_dataset_subfolder, 
-                           domain=domain, aggregation=aggregation, scenario=scenario, lraw=False,)
-        print(ds,'\n\n')
-
-        from geodata.base import Variable
-        from processing.newvars import computePotEvapPM
-        # compute PET
-        pet,rad,wnd = computePotEvapPM(ds, lterms=True, lmeans=True, lrad=True, lgrdflx=False, lpmsl=True)
-        pet.atts.long_name = 'Potential Evapotranspiration'
-        rad.atts.long_name = 'Radiation Term of PET'
-        wnd.atts.long_name = 'Wind Term of PET'
-        # add to dataset
-        print(pet,'\n\n')        
-
-        exit()
-        ds.addVariable(pet, asNC=True, copy=True)
-
-        # compute liquid water flux
-        dswe = np.gradient(ds.snow.data_array, axis=None)
-        dt = np.gradient(ds.time.data_array, axis=None)*86400.
-        assert 'days' in ds.time.units, ds.time
-        data = np.clip(ds.precip.data_array - dswe/dt, a_min=0, a_max=None)
-        lwf = Variable(name='liqwatflx', units=ds.precip.units, axes=ds.precip.axes, data=data, 
-                       long_name='Liquid Water Flux')
-        # add to dataset
-        print(lwf,'\n\n')        
-        ds.addVariable(lwf, asNC=True, copy=True)
+                # load data using GeoPy/NetCDF4
+                pet_varlist = ['T2','Tmin','Tmax','pmsl','zs','q2','U10','SWDNB','SWUPB','LWDNB','LWUPB']
+                lwf_varlist = ['snow','precip']
+                #dataset = 'CanESM-CRCM5'; scenario = 'historical'
+                ds = loadCORDEX_TS(dataset=dataset, grid=station_name, varlist=lwf_varlist+pet_varlist, 
+                                   lxarray=False, load=True, mode='rw',
+                                   dataset_subfolder=station_dataset_subfolder, 
+                                   domain=domain, aggregation=aggregation, scenario=scenario, lraw=False,)
+                #print(ds,'\n\n')
         
-        # save dataset
-        ds.sync()
-        print(ds)
-        ds.close()
+                from geodata.base import Variable
+                from processing.newvars import computePotEvapPM
+                # compute PET
+                pet,rad,wnd = computePotEvapPM(ds, lterms=True, lmeans=True, lrad=True, lgrdflx=False, lpmsl=True)
+                pet.data_array = np.clip(pet.data_array, a_min=0, a_max=None) # remove negative PET
+                pet.atts.long_name = 'Potential Evapotranspiration'
+                rad.atts.long_name = 'Radiation Term of PET'
+                wnd.atts.long_name = 'Wind Term of PET'
+                # add to dataset
+                #print(pet,'\n\n')        
+                ds.addVariable(pet, asNC=True, copy=True)
+        
+        
+                # compute liquid water flux
+                dswe = np.gradient(ds.snow.data_array, axis=None)
+                dt = np.gradient(ds.time.data_array, axis=None)*86400.
+                assert 'days' in ds.time.units, ds.time
+                data = np.clip(ds.precip.data_array - dswe/dt, a_min=0, a_max=None)
+                lwf = Variable(name='liqwatflx', units=ds.precip.units, axes=ds.precip.axes, data=data, 
+                               long_name='Liquid Water Flux')
+                # add to dataset
+                #print(lwf,'\n\n')        
+                ds.addVariable(lwf, asNC=True, copy=True)
+                
+                # save dataset
+                ds.sync()
+                print(ds)
+                ds.close()
+                
     
     elif mode == 'load_timeseries':
        
@@ -432,6 +445,8 @@ if __name__ == '__main__':
             for scenario in scenarios:
                 
                 print("\n   ***   ",dataset,scenario,"   ***   \n")
+                
+                # load raw data using xarray
                 xds = loadCORDEX_Raw(dataset=dataset, varlist=None, decode_times=False,
                                      domain=domain, aggregation=aggregation, scenario=scenario,
                                      lgeoref=False, lraw=True)

src/geodata/netcdf.py
@@ -259,7 +259,10 @@ class VarNC(Variable):
         # set slices to None, since they unneccessary now, and cause problems when slicing
         self.slices = None
       # finally, get data!
-      data = self.ncvar.__getitem__(slcs) # exceptions handled by netcdf module
+      if self.ndim == 0 and self.ncvar.ndim == 0:
+          data = self.ncvar.getValue() # special case for scalars
+      else:
+          data = self.ncvar.__getitem__(slcs) # exceptions handled by netcdf module
       if self.dtype is not None and not np.issubdtype(data.dtype,self.dtype):
         if 'scale_factor' in self.ncvar.ncattrs():
             self.dtype = data.dtype # data was scaled automatically in NetCDF module
@@ -533,7 +536,8 @@ class DatasetNetCDF(Dataset):
   
   def __init__(self, name=None, title=None, dataset=None, filelist=None, varlist=None, variables=None,
       	       varatts=None, atts=None, axes=None, multifile=False, check_override=None, ignore_list=None, 
-               folder='', mode='r', ncformat='NETCDF4', squeeze=True, load=False, check_vars=None):
+               folder='', mode='r', ncformat='NETCDF4', squeeze=True, lscalars=False, load=False, 
+               check_vars=None):
     ''' 
       Create a Dataset from one or more NetCDF files; Variables are created from NetCDF variables. 
       Alternatively, create a netcdf file from an existing Dataset (Variables can be added as well).  
@@ -553,6 +557,7 @@ class DatasetNetCDF(Dataset):
         mode           : file mode: whether read ('r') or write ('w') actions are intended/permitted (string; passed to netCDF4.Dataset)
         ncformat       : format of NetCDF file, i.e. NETCDF3 NETCDF4 or NETCDF_CLASSIC (string; passed to netCDF4.Dataset)
         squeeze        : squeeze singleton dimensions from all variables
+        lscalars       : load scalar variables or skip (default: skip)
         load           : load data from disk immediately (passed on to VarNC)
                        
       NetCDF Attributes:
@@ -705,7 +710,7 @@ class DatasetNetCDF(Dataset):
         for var in dsvars:
           ncvar= ds.variables[var]
           if var in axes: pass # do not treat coordinate variables as real variables 
-          elif ncvar.ndim == 0: pass # also ignore scalars for now...
+          elif ncvar.ndim == 0 and not lscalars: pass # also ignore scalars for now...
           elif var in variables: # if already present, make sure variables are essentially the same
             varobj = variables[var] 
             if var not in check_override:

src/processing/newvars.py
@@ -172,6 +172,9 @@ def computePotEvapPM(dataset, lterms=True, lmeans=False, lrad=True, lgrdflx=True
   # else: Es = e_sat(T) # backup, but not very accurate
   else: raise VariableError("'Tmin' and 'Tmax' are required to compute saturation water vapor pressure for PET calculation.")
   D = Delta(T) # slope of saturation vapor pressure w.r.t. temperature
+  # determine reference variable (see what's available)
+  for refvar in ('ps','T2','Tmax','Tmin','Q2','q2','U10','u10',):
+      if refvar in dataset: break
   # compute potential evapotranspiration according to Penman-Monteith method 
   # (http://www.fao.org/docrep/x0490e/x0490e06.htm#fao%20penman%20monteith%20equation)
   if lterms:
@@ -179,15 +182,15 @@ def computePotEvapPM(dataset, lterms=True, lmeans=False, lrad=True, lgrdflx=True
     rad = evaluate('0.0352512 * D * (Rn + G) / Dgu') # radiation term
     wnd = evaluate('g * u2 * (es - ea) * 0.9 / T / Dgu') # wind term (vapor deficit)
     pet = evaluate('( 0.0352512 * D * (Rn + G) + ( g * u2 * (es - ea) * 0.9 / T ) ) / ( D + g * (1 + 0.34 * u2) ) / 86400')
-    import numpy as np
     assert np.allclose(pet, rad+wnd, equal_nan=True)
-    rad = Variable(data=rad, name='petrad', units='kg/m^2/s', axes=dataset['ps'].axes)
-    wnd = Variable(data=wnd, name='petwnd', units='kg/m^2/s', axes=dataset['ps'].axes)
+    rad = Variable(data=rad, name='petrad', units='kg/m^2/s', axes=dataset[refvar].axes)
+    wnd = Variable(data=wnd, name='petwnd', units='kg/m^2/s', axes=dataset[refvar].axes)
   else:
     pet = evaluate('( 0.0352512 * D * (Rn + G) + ( g * u2 * (es - ea) * 0.9 / T ) ) / ( D + g * (1 + 0.34 * u2) ) / 86400')
   # N.B.: units have been converted to SI (mm/day -> 1/86400 kg/m^2/s, kPa -> 1000 Pa, and Celsius to K)
-  pet = Variable(data=pet, name='pet', units='kg/m^2/s', axes=dataset['ps'].axes)
-  assert 'waterflx' not in dataset or pet.units == dataset['waterflx'].units, pet
+  pet = Variable(data=pet, name='pet', units='kg/m^2/s', axes=dataset[refvar].axes)
+  assert 'liqwatflx' not in dataset or pet.units == dataset['liqwatflx'].units, pet
+  assert 'precip' not in dataset or pet.units == dataset['precip'].units, pet
   # return new variable(s)
   return (pet,rad,wnd) if lterms else pet
 